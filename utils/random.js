4dab0aecee8010d3c3d7827d12ed9ceabd9ad6d7
randfur <randfur@gmail.com>
2022-04-25 13:57:12 +1000

    randomRange

--- utils/random.js
+++ utils/random.js
@@ -2,6 +2,10 @@ export function random(x) {
   return Math.random() * x;
 }
 
+export function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 export function deviate(x) {
   return (Math.random() * 2 - 1) * x;
 }
