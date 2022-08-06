2768f790af5c306731420042a7de8af1286b1972
Alan Cutter <randfur@gmail.com>
2022-08-06 12:26:13 +1000

    bark

--- tests/manual/basic/main.js
+++ tests/manual/basic/main.js
@@ -34,8 +34,6 @@ class BlueScene extends BasicScene {
         colour: 'blue',
       });
     }
-
-    await this.forever();
   }
 }
 
@@ -51,8 +49,6 @@ class RedScene extends BasicScene {
         colour: 'red',
       });
     }
-
-    await this.forever();
   }
 }
 
@@ -68,7 +64,6 @@ class TestEntity extends BasicEntity {
   }
 
   async body() {
-
     let dx = deviate(4);
     let dy = deviate(4);
 
@@ -78,10 +73,6 @@ class TestEntity extends BasicEntity {
       const {width, height} = this.game;
       this.collider.x += dx;
       this.collider.y += dy;
-
-      // const dampening = 0.98;
-      // dx = dx * dampening + Math.sin(this.collider.y);
-      // dy = dy * dampening + Math.sin(this.collider.x);
 
       const size = this.size;
       if (this.collider.x < -size) { this.collider.x += width + size; }
