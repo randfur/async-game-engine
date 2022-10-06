b3be3ab3dcd043db000f40fdbfab2829a2328b41
Alan Cutter <alancutter@chromium.org>
2022-10-07 08:50:42 +1100

    wip

--- presets/basic-entity.js
+++ presets/basic-entity.js
@@ -16,22 +16,43 @@ export class BasicEntity extends Entity {
     this.sprite = null;
   }
 
+  onInput(eventName, event) {}
+
   enableCollisions() {
     this.collider = this.scene.collision2dRegistry.register(
       this,
-      collisionNode => {
-        if (!this.sprite) {
-          return false;
-        }
-        this.sprite.updateCollisionNode(collisionNode, this.transform);
-        return true;
-      },
+      collisionNode => this.updateCollisionNode(collisionNode),
       otherCollider => this.onCollision(otherCollider),
     );
     this.collider.transform = this.transform;
   }
 
-  onInput(eventName, event) {}
+  updateCollisionNode(collisionNode) {
+    if (!this.sprite) {
+      return false;
+    }
+  static #collisionBoundingBoxPoints = [new Vec2(), new Vec2(), new Vec2(), new Vec2()];
+  static #collisionMatrix = new Mat3();
+  updateCollisionNode(collisionNode, entityTransform) {
+    const [a, b, c, d] = Sprite.#collisionBoundingBoxPoints;
+    a.set(0, 0);
+    b.set(this.image.width, 0);
+    c.set(this.image.width, this.image.height);
+    d.set(0, this.image.height);
+    Sprite.#collisionMatrix.setIdentity();
+    this.transform.applyToMatrix(Sprite.#collisionMatrix);
+    entityTransform.applyToMatrix(Sprite.#collisionMatrix);
+    for (const vector of Sprite.#collisionBoundingBoxPoints) {
+      Sprite.#collisionMatrix.applyToVector(vector);
+    }
+    collisionNode.x = Math.min(a.x, b.x, c.x, d.x);
+    collisionNode.y = Math.min(a.y, b.y, c.y, d.y);
+    collisionNode.width = Math.max(a.x, b.x, c.x, d.x) - collisionNode.x;
+    collisionNode.height = Math.max(a.y, b.y, c.y, d.y) - collisionNode.y;
+  }
+
+    return true;
+  }
 
   onCollision(otherCollider) {}
 
@@ -45,14 +66,14 @@ export class BasicEntity extends Entity {
     }
   }
 
+  static #drawSpriteMatrix = new Mat3();
   drawSprite(context, sprite) {
-    const matrix = Mat3.pool.acquire();
+    const matrix = BasicEntity.#drawSpriteMatrix;
     matrix.setIdentity();
     sprite.transform.applyToMatrix(matrix);
     this.transform.applyToMatrix(matrix);
     this.scene.cameraTransform.applyToMatrix(matrix);
     matrix.applyToContext(context);
-    Mat3.pool.release(1);
     context.drawImage(sprite.image, 0, 0);
   }
 }
