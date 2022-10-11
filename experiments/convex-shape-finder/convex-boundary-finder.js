6b05978f9b311806bceab3b080f9aa4b4c6f92a2
Alan Cutter <alancutter@chromium.org>
2022-10-11 22:00:34 +1100

    top and bottom

--- experiments/convex-shape-finder/convex-boundary-finder.js
+++ experiments/convex-shape-finder/convex-boundary-finder.js
@@ -18,17 +18,53 @@ export class ConvexBoundaryFinder extends BasicEntity {
   }
 
   async findLines() {
-    const heightMap = await this.buildHeightMap();
+    const topHeightMap = await this.buildHeightMap(/*top=*/true);
+    const bottomHeightMap = await this.buildHeightMap(/*top=*/false);
 
-    heightMap.sort((a, b) => b.height - a.height);
-    this.inProgressSortedHeightMap = heightMap;
+    topHeightMap.sort((a, b) => b.height - a.height);
+    bottomHeightMap.sort((a, b) => b.height - a.height);
+    this.debugTopHeightMap = topHeightMap;
+    this.debugBottomHeightMap = bottomHeightMap;
 
-    const points = await this.extractConvexPoints(heightMap);
+    const topPoints = await this.extractConvexPoints(topHeightMap, /*top=*/true);
+    const bottomPoints = await this.extractConvexPoints(bottomHeightMap, /*top=*/false);
   }
 
-  async extractConvexPoints(heightMap) {
+  async buildHeightMap(top) {
+    const heightMap = [];
+    const imageData = this.picture.imageData;
+    let lastHeight = 0;
+    for (let x = 0; x <= imageData.width; ++x) {
+      const thisHeight = x < imageData.width ? this.getHeight(x, top) : lastHeight;
+      const height = Math.max(lastHeight, thisHeight);
+      if (height > 0) {
+        heightMap.push({x, height});
+      }
+      lastHeight = thisHeight;
+    }
+    return heightMap;
+  }
+
+  getHeight(x, top) {
+    const imageData = this.picture.imageData;
+    for (let height = imageData.height; height > 0; --height) {
+      const y = top ? imageData.height - height : height - 1;
+      const index = y * this.picture.imageData.width * 4 + x * 4;
+      if (this.picture.imageData.data[index + 3] > 0) {
+        return height;
+      }
+    }
+    return 0;
+  }
+
+  isOpaque(x, y) {
+    const index = y * this.picture.imageData.width * 4 + x * 4;
+    return this.picture.imageData.data[index + 3] > 0;
+  }
+
+  async extractConvexPoints(heightMap, top) {
     const points = [];
-    this.inProgressPoints = points;
+    this[top ? 'debugTopPoints' : 'debugBottomPoints'] = points;
     for (const item of heightMap) {
       await this.tick();
 
@@ -96,58 +132,35 @@ export class ConvexBoundaryFinder extends BasicEntity {
     return points;
   }
 
-  async buildHeightMap() {
-    const heightMap = [];
-    const imageData = this.picture.imageData;
-    let lastHeight = 0;
-    for (let x = 0; x <= imageData.width; ++x) {
-      const thisHeight = x < imageData.width ? this.getHeight(x) : lastHeight;
-      const height = Math.max(lastHeight, thisHeight);
-      if (height > 0) {
-        heightMap.push({x, height});
-      }
-      lastHeight = thisHeight;
-    }
-    return heightMap;
-  }
-
-  getHeight(x) {
-    const imageData = this.picture.imageData;
-    for (let height = imageData.height; height > 0; --height) {
-      const y = imageData.height - height;
-      const index = y * this.picture.imageData.width * 4 + x * 4;
-      if (this.picture.imageData.data[index + 3] > 0) {
-        return height;
-      }
-    }
-    return 0;
-  }
-
-  isOpaque(x, y) {
-    const index = y * this.picture.imageData.width * 4 + x * 4;
-    return this.picture.imageData.data[index + 3] > 0;
-  }
-
   onDraw(context, width, height) {
+    const imageHeight = this.picture.imageData.height;
     context.resetTransform();
     const bottom = Vec2.pool.acquire();
-    bottom.set(0, this.picture.canvas.height);
+    bottom.set(0, imageHeight);
     this.picture.sprite.transform.applyToVector(bottom);
     this.picture.transform.applyToVector(bottom);
 
-    if (this.inProgressSortedHeightMap) {
-      for (const [i, {x, height}] of enumerate(this.inProgressSortedHeightMap)) {
-        const alpha = (1 - (i / (this.inProgressSortedHeightMap.length - 1))) / 2;
+    if (this.debugTopHeightMap) {
+      for (const [i, {x, height}] of enumerate(this.debugTopHeightMap)) {
+        const alpha = (1 - (i / (this.debugTopHeightMap.length - 1))) / 2;
         context.fillStyle = `rgba(255, 0, 0, ${alpha})`;
-        context.fillRect(bottom.x + x, bottom.y - height, 1, height);
+        context.fillRect(bottom.x + x, bottom.y - height, 1, 10);
       }
     }
 
-    if (this.inProgressPoints) {
+    if (this.debugTopHeightMap) {
+      for (const [i, {x, height}] of enumerate(this.debugBottomHeightMap)) {
+        const alpha = (1 - (i / (this.debugBottomHeightMap.length - 1))) / 2;
+        context.fillStyle = `rgba(0, 255, 0, ${alpha})`;
+        context.fillRect(bottom.x + x, bottom.y - imageHeight + height - 10, 1, 10);
+      }
+    }
+
+    if (this.debugTopPoints) {
       context.strokeStyle = 'red';
       context.beginPath();
       let first = true;
-      for (const {x, height} of this.inProgressPoints) {
+      for (const {x, height} of this.debugTopPoints) {
         if (first) {
           context.moveTo(bottom.x + x, bottom.y - height);
           first = false;
@@ -157,8 +170,27 @@ export class ConvexBoundaryFinder extends BasicEntity {
       }
       context.stroke();
       context.fillStyle = 'white';
-      for (const {x, height} of this.inProgressPoints) {
+      for (const {x, height} of this.debugTopPoints) {
         context.fillRect(bottom.x + x, bottom.y - height, 1, 1);
+      }
+    }
+
+    if (this.debugBottomPoints) {
+      context.strokeStyle = 'lime';
+      context.beginPath();
+      let first = true;
+      for (const {x, height} of this.debugBottomPoints) {
+        if (first) {
+          context.moveTo(bottom.x + x, bottom.y - imageHeight + height);
+          first = false;
+        } else {
+          context.lineTo(bottom.x + x, bottom.y - imageHeight + height);
+        }
+      }
+      context.stroke();
+      context.fillStyle = 'white';
+      for (const {x, height} of this.debugBottomPoints) {
+        context.fillRect(bottom.x + x, bottom.y - imageHeight + height, 1, 1);
       }
     }
 
