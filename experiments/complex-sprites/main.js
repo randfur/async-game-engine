fa4f3549a3d2c8e9df9e9b9ff1fbe4cd343ac5a6
Alan Cutter <alancutter@chromium.org>
2022-10-20 21:27:17 +1100

    Bark dog

--- experiments/complex-sprites/main.js
+++ experiments/complex-sprites/main.js
@@ -1,6 +1,50 @@
 import {Game} from '../../engine/game.js';
 import {BasicScene} from '../../presets/basic-scene.js';
 
+/*
+struct SpriteInstance {
+  spritePack: SpritePack,
+  sprite: Sprite,
+  frameIndex: u32,
+  elapsedFrames: u32,
+  spriteStartTime: f64,
+  transform: Transform,
+}
+
+struct SpritePack {
+  sprites: Map<String, Sprite>,
+  graphics: Map<String, Graphic>,
+}
+
+struct Sprite {
+  name: String,
+  frames: Vec<Frame>,
+  framesPerSecond: u32,
+  switchTo: String,
+}
+
+struct Frame {
+  graphicReference: String,
+  frameDelay: u32,
+}
+
+struct Graphic {
+  name: String,
+  parts: Vec<Part>,
+}
+
+enum Part {
+  Image {
+    bitmap: Bitmap,
+    transform: Transform,
+  },
+  GraphicReference {
+    name: String,
+    transform: Transform,
+  },
+}
+*/
+
 async function main() {
   new Game({
     initialScene: class extends BasicScene {
@@ -10,40 +54,3 @@ async function main() {
   });
 }
 main();
-
-/*
-struct SpriteInstance {
-  spritePack: SpritePack,
-  sprite: Sprite,
-  frameIndex: usize,
-  frameStartTime: f64,
-  transform: Transform,
-}
-
-type SpritePack = Map<String, Sprite>
-
-struct Sprite {
-  name: String,
-  frames: Vec<Frame>,
-  switchTo: String,
-}
-
-struct Frame {
-  parts: Vec<Part>,
-  duration: f32,
-}
-
-struct TransformedPart {
-  transform: Transform,
-  part: Part,
-}
-
-enum Part {
-  Image {
-    bitmap: Bitmap,
-  },
-  Reference {
-    name: String,
-  },
-}
-*/
