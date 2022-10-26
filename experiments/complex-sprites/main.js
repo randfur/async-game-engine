73b209759396ffb7c24989b98d69fc895b3763c3
Alan Cutter <alancutter@chromium.org>
2022-10-26 22:18:35 +1100

    barksprite

--- experiments/complex-sprites/main.js
+++ experiments/complex-sprites/main.js
@@ -1,5 +1,6 @@
 import {Game} from '../../engine/game.js';
 import {BasicScene} from '../../presets/basic-scene.js';
+import {BasicEntity} from '../../presets/basic-entity.js';
 
 /*
 interface SpriteInstance {
@@ -11,10 +12,7 @@ interface SpriteInstance {
   transform: Transform;
 }
 
-interface SpritePack {
-  sprites: Map<string, Sprite>;
-  graphics: Map<string, Graphic>;
-}
+type SpritePack = Map<string, Sprite>;
 
 interface Sprite {
   name: string;
@@ -24,8 +22,9 @@ interface Sprite {
 }
 
 interface Frame {
-  graphicReference: string;
+  graphic: Graphic;
   frameDelay: u32;
+  convexCollisionPolygon: Vec2[];
 }
 
 interface Graphic {
@@ -35,22 +34,82 @@ interface Graphic {
 
 enum Part {
   Image {
-    bitmap: Bitmap;
+    src: string;
     transform: Transform;
-  };
+  },
   GraphicReference {
-    name: string;
+    spriteName: string;
+    spriteFrame: u32;
     transform: Transform;
   },
 }
+
+interface Transform {
+  parent: Transform? | null;
+  origin: Vec2;
+  scale: Vec2;
+  rotate: Vec2;
+  translate: Vec2;
+},
 */
 
 async function main() {
   new Game({
     initialScene: class extends BasicScene {
       async run() {
+        this.create(Dog);
       }
     },
   });
 }
 main();
+
+class Dog extends BasicEntity {
+  init() {
+    // SpriteRegistry
+    // - preloadPacks(srcs)
+    // - Loaded packs.
+    // - Active handles.
+    // SpriteHandle
+    // - Contains sprite instance.
+    // - switchTo(spriteName)
+    // - loadPack(src, spriteName)
+    // - draw(context)
+    // - entityTransform
+    // - cameraTransform
+    this.spritePack = {
+      default: {
+        name: 'stand',
+        frames: [
+          {
+            graphic: {
+            },
+            frameDelay: 20,
+            convexCollisionPolygon: [],
+          },
+          {
+            graphic: {
+            },
+            frameDelay: 1,
+            convexCollisionPolygon: [],
+          },
+        ],
+        framesPerSecond: 12,
+        switchTo: null,
+      },
+      bark: {
+        name: 'bark',
+        frames: [
+        ],
+        framesPerSecond: 12,
+        switchTo: 'default',
+      },
+    };
+  }
+
+  async run() {
+  }
+
+  onDraw(context, width, height) {
+  }
+}
